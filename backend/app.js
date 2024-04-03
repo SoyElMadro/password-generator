a779cbbc819d92be3fda6eab163223fa31fef99b
SoyElMadro <gonza271106madro@gmail.com>
2024-04-02 23:25:56 -0300

    fixed

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST')
+const allowCors = (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
+  );
   if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
+    res.sendStatus(200);
+    return;
   }
-  return await fn(req, res)
-}
+  next();
+};
 
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
+app.use(allowCors);
 
 app.post('/api/generate-password', (req, res) => {
   const { length, includeUppercase, includeNumbers, includeSymbols } = req.body;
