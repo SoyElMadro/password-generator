487d12244ee6606ee12dc22ba184ba14d3ddb194
SoyElMadro <gonza271106madro@gmail.com>
2024-04-02 23:07:21 -0300

    fixed an slash error

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://password-generator-red-iota.vercel.app/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://password-generator-red-iota.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') {
