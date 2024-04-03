a3f3b94f4fc5d2b0d02341a60fd85f6982273730
SoyElMadro <gonza271106madro@gmail.com>
2024-04-02 23:03:07 -0300

    fixed cors error

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,16 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://password-generator-red-iota.vercel.app/');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.post('/api/generate-password', (req, res) => {
   const { length, includeUppercase, includeNumbers, includeSymbols } = req.body;
 
