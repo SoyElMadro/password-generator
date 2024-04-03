9e06c0ee903f310af4a3979c4f82fd277d7b7c82
SoyElMadro <gonza271106madro@gmail.com>
2024-04-02 23:21:04 -0300

    updated middleware

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 const allowCors = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST')
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
